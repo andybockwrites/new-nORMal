00769298ff3b234d37b56e8ddf47792dc6ca8580
Andy Bock <andybockwrites@gmail.com>
2023-06-06 15:47:13 -0400

    pushing code

--- Develop/models/index.js
+++ Develop/models/index.js
@@ -5,12 +5,28 @@ const Tag = require('./Tag');
 const ProductTag = require('./ProductTag');
 
 // Products belongsTo Category
+Product.belongsTo(Category);
 
 // Categories have many Products
+Category.haveMany(Product);
 
 // Products belongToMany Tags (through ProductTag)
+Product.belongToMany(Tag, {
+  through: {
+    model: ProductTag,
+    unique: false
+  },
+  as: 'products'
+});
 
 // Tags belongToMany Products (through ProductTag)
+Tag.belongToMany(Product, {
+  through: {
+    model: ProductTag,
+    unique: false
+  },
+  as: 'product_tags'
+});
 
 module.exports = {
   Product,
